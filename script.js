414af6e72ed83afdc07aeaac24d647df04d09523
Wren Purcell <wpurcell@theknowledgehouse.org>
2024-07-30 21:07:29 +0000

    added comments

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//console log text, expression 
+
+// Create a variable to hold your name as a string
+
+//Concatenate your name variable into a message and log it to the console.
+//Example: “Hello, Zach!” 
+
+//console log your name variable
+
+//Re-assign the value of your name variable below its declaration and console log it again
+
+//practice math operators
+
+// boolean - true or false
+
+//create null and undefined
+
+//mix data types
+
+//string methods
+
+//link up html file and create prompt and save value
+
+//access the DOM
